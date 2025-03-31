0d8c1f37bbed84b943ed0dfbbc373ce9b09a4267
GitHub Action <action@github.com>
2025-03-31 16:37:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/doqv/visualizerjetbrainsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/doqv/visualizerjetbrainsplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.doqv.visualizerjetbrainsplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.doqv.visualizerjetbrainsplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
